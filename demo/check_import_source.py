2ec425ad0b0e428244161e7d20cbb636db039d71
Sritay Mistry <sritaymistry@gmail.com>
2026-01-12 21:12:20 +0530

    Cleanup: Move demo, old configs, and temp files to root_archive

--- demo/check_import_source.py
+++
@@ -1,7 +0,0 @@
-
-import sys
-import metastategen.models.diffusion as diffusion
-
-print("--- Import Source Check ---")
-print(f"sys.path: {sys.path}")
-print(f"diffusion file: {diffusion.__file__}")
